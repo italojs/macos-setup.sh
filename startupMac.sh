6678bb6ab833b7e276768e974732ace5e1a825d4
Italo Jose <italo.i@live.com>
2020-08-04 22:19:08 -0300

    fix syntax errors | add clear steps

--- startupMac.sh
+++ startupMac.sh
@@ -1,6 +1,5 @@
 GREEN="\033[0;32m"
 NC="\033[0m" # No Color
-DARK_GREY="\033[1;30m"
 LIGHT_GREY="\033[0;37m"
 YELLOW="\033[1;33m"
 
@@ -17,22 +16,26 @@ INSTALL_APP(){
     read input
     if echo "$input" | grep -iq "^y" ;then
         PRINT "Installing ${GREEN} ${APP} ${NC}..."
-        brew install $CASK $APP
+        brew $CASK install $APP
     fi
+    clear
 }
 
-PRINT "${YELLOW}Are you have installed 'Apple's Command Line Tools' before run this script? (y/n)"
-if echo "$input" | grep -iq "^y" ;then
+PRINT "${YELLOW}Are you have installed Apple's Command Line Tools before run this script? (y/n)"
+read input
+if echo $input | grep -iq "^y" ;then
 	PRINT "Niiice! Let's continue configuring your beautiful Macbook! :D"
 else
 	PRINT "Please, run 'xcode-select --install' before this script"
-
+fi
 
 PRINT "Installing Homebrew..."
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+clear
 
 PRINT "Updating zsh..."
 brew install zsh
+clear
 
 # This session was copied from github.com/ErickWendel/ew-ubuntu-setup
 PRINT "What name do you want to use in GIT user.name?"
@@ -52,6 +55,7 @@ read input
 if echo "$input" | grep -iq "^y" ;then
 	git config --global core.editor vim
 fi
+clear
 
 PRINT "Generating a SSH Key..."
 ssh-keygen -t rsa -b 4096 -C $git_config_user_email
@@ -60,16 +64,20 @@ cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
 
 PRINT "Enabling workspaces for both screens..."
 gsettings set org.gnome.mutter workspaces-only-on-primary false
+clear
 
-PRINT "Installing Oh-My-Zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#PRINT "Installing Oh-My-Zsh..."
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+clear
 
 PRINT "Do you want to configure your terminal with powerlevel10k? (y/n)"
-PRINT "${DARK_GREY}know more about powerlevel10k in ${LIGHT_GREY} https://github.com/romkatv/powerlevel10k"
-if echo "$input" | grep -iq "^y" ;then
+PRINT "${LIGHT_GREY}know more about powerlevel10k in https://github.com/romkatv/powerlevel10k"
+read input
+if echo $input | grep -iq "^y" ;then
     brew install romkatv/powerlevel10k/powerlevel10k
     echo "source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme" >>! ~/.zshrc
 fi
+clear
 
 INSTALL_APP "node"
 INSTALL_APP "docker" "cask"
